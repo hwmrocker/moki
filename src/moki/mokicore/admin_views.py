fca7c6a6f41275252e8dc719ce7c74defd3e8fd3
Olaf Gladis <olaf@gladis.org>
2012-04-11 08:30:11 +0200

    WIP cover download geht noch nicht

--- src/moki/mokicore/admin_views.py
+++ src/moki/mokicore/admin_views.py
@@ -4,6 +4,10 @@ from views import render_to_response
 from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 from datetime import datetime
+import imdb
+import urllib2
+from urlparse import urlparse
+from django.core.files import File
 
 import logging
 logger = logging.getLogger('custom')
@@ -12,12 +16,10 @@ dbg = logger.debug
 from django import forms
 
 class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=50)
     file  = forms.FileField()
 
 
 def movie_upload(request):
-    dbg(request)
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
@@ -28,6 +30,21 @@ def movie_upload(request):
     return render_to_response('upload.html', {'form': form}, request=request)
 
 def handle_uploaded_file(fileh):
+    q = imdb.IMDb()
     dbg(len(fileh))
     for line in fileh.readlines():
-        dbg(line)
+        mid, date, thema, aktion = line.split('|')
+        m = q.get_movie(mid)
+        newf = Film()
+        newf.titel = m['title']
+        newf.thema = thema
+        newf.aktions_titel = aktion
+        newf.imdb_id = mid
+        newf.description = ''
+        newf.date = datetime.strptime(date,'%d.%m.%Y')
+        cover_url = m.get('full-size cover url')
+        if cover_url:
+            newf.cover.save(date+'.jpg',
+                            File(urllib2.urlopen(cover_url).read()),
+                            save=True)
+        newf.save()
